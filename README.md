b7be1eb3be6e84109b8fb1bc155c363d54eaeab3
Adi Gunawan Hidayat <adigunawanhidayat29@gmail.com>
2024-01-23 17:22:57 +0700

    add readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# Al-Quran Web App
+
+This is a Progressive Web App (PWA) for reading Al-Quran. It allows users to navigate and read Quranic verses with ease.
+
+## Demo
+
+Visit the live demo: [Al-Quran Web App](https://quran.aghansolusiutama.com/)
+
+## Features
+
+- Navigate through Quranic pages using a user-friendly interface.
+- Search and select specific surahs for quick access.
+- Save and track the last-read page using local storage.
+- Offline mode: Access the app even without an internet connection.
+
+## Getting Started
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/aghidayat/quran-halaman-kemenag.git
--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,51 @@
-const cacheName = 'al-quran-app-cache-v1';
+const cacheName = "al-quran-app-cache-v1";
 const filesToCache = [
-  '/',
-  'path/to/icon.png', // Replace with the actual path to your app icon
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+  "/",
+  "/assets/favicon-32x32.png", // Replace with the actual path to your app icon
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+  "https://code.jquery.com/jquery-3.5.1.slim.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js",
+  "/assets",
+  "/images",
   // Add other URLs that need to be cached
 ];
 
-self.addEventListener('install', (e) => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
       return cache.addAll(filesToCache);
-    })
+    }),
   );
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches
+      .match(e.request)
+      .then((response) => {
+        return (
+          response ||
+          fetch(e.request).then((fetchResponse) => {
+            if (
+              !fetchResponse ||
+              fetchResponse.status !== 200 ||
+              fetchResponse.type !== "basic"
+            ) {
+              return fetchResponse;
+            }
+
+            const responseToCache = fetchResponse.clone();
+
+            caches.open(cacheName).then((cache) => {
+              cache.put(e.request, responseToCache);
+            });
+
+            return fetchResponse;
+          })
+        );
+      })
+      .catch(() => {
+        return caches.match("/");
+      }),
   );
 });
